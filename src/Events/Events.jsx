5b9ad8d1f2900ea7838653521b4842694570d868
abhinash1051 <abhinashkumar03619@gmail.com>
2023-11-23 11:22:16 +0530

    event

--- src/Events/Events.jsx
+++ src/Events/Events.jsx
@@ -2,151 +2,151 @@ import React from 'react'
 import './Events.css'
 import event from '../assests/event.jpeg'
 export default function Events() {
-  return (
-    <div className="container-event">
-        <header>
-            <h1>EVENTS</h1>
-        </header>
-        <div className="events-list">
+    return (
+        <div className="container-event">
+            <header>
+                <h1>EVENTS</h1>
+            </header>
+            <div className="events-list">
 
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 2" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 2" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 2" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 2" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 2" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 2" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 2" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 2" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 1" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
+
+                <div className="card-event">
+                    <a href="#" target="_blank">
+                        <img src={event} alt="Event 2" />
+                    </a>
+                    {/* <div className="info"></div> */}
+                </div>
             </div>
-    
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 2"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-    
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 2"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-    
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 2"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-    
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 2"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-    
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 2"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-    
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 2"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-    
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 2"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-    
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 2"/>
-                </a>
-                <div className="info"></div>
-            </div>
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 1"/>
-                </a>
-                <div className="info"></div>
-            </div>
-    
-            <div className="card-event">
-                <a href="#" target="_blank">
-                    <img src={event} alt="Event 2"/>
-                </a>
-                <div className="info"></div>
-            </div>
+
+
         </div>
-        
-
-    </div>
-  )
+    )
 }
