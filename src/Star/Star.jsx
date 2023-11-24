8c774cf2a3d39e7ce832be0784667c9f3d1b4dcf
sanchitaa14 <sakshibhardwaj14@gmail.com>
2023-11-25 00:40:44 +0530

    well

--- src/Star/Star.jsx
+++ src/Star/Star.jsx
@@ -2,33 +2,45 @@ import React from 'react'
 import "./Star.css"
 import Akhil from "../assests/Akhil.jpg"
 import location from "../assests/location.png"
-import calender from "../assests/calendar-icon.png"
+import calender from "../assests/calender-icon.png"
 import clock from "../assests/clock.png"
+import page from "../assests/page1.jpg"
+import page2 from "../assests/page2.jpg"
+import solo from "../assests/solo.png"
 export default function Star() {
   return (
     <div className='starpage'>
         <div className='content-star'>
         <img className="star-bg" src={Akhil}/>
+        
         <p>Akhil Sachdeva, the acclaimed Indian musician, singer, and composer, gained recognition as the lead vocalist of the 
             Delhi-based band Nasha in 2009. His Bollywood journey took flight when his song "Humsafar" was featured in the film 
             "Badrinath Ki Dulhania." Graduating with Honours in Economics from Delhi College of Arts & Commerce, University of Delhi,
              Sachdeva's versatile talent shines through in his globally acclaimed track "Tera Ban Jaunga" from the film "Kabir Singh." 
              Managed by Mourjo Chatterjee of On Stage Talents, he continues to captivate audiences with his emotive vocals 
              and impactful compositions, solidifying his position in the realm of music.</p>
+            
             <div className='image-icon'>
                 <div>
-            <img className="star-icons" src={location}/>
+            <img className="star-icons" src={location}/>   BPIT
             </div>
             <div>
-            <img className="star-icons" src={clock}/>
+            <img className="star-icons" src={clock}/>    7:00 p.m.
             
             </div>
             <div>
-            <img className="star-icons" src={calender}/>
+            <img className="star-icons" src={calender}/>    1st December
+            </div>
+            <div>
+              <button className='pass-button'>Get Your Passes</button>
             </div>
             </div>
             </div>
-            <h1> Akhil Sachdeva</h1>
+            <div className='main-content'>
+              <img className="side-pic" src = {page}/>
+            <h1>Akhil</h1>
+            <img className='side-pic' src = {page2}/>
+            </div>
             
     </div>
   )
